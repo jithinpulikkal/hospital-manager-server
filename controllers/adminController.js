517a0363b79033cc558518781fcb54d09462e6d6
Jithin <jithinpulikkal875@gmail.com>
2024-01-09 18:36:32 +0530

    edit data

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -215,7 +215,7 @@ module.exports = {
             if (!user) {
                 return res.status(404).json({ error: "User not found" });
             }
-            res.status(200).json({ employee: user });
+            res.json(user);
             
         } catch (error) {
             console.error("Error fetching user data:", error);
